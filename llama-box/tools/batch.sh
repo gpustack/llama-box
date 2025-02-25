adfe6502625cd8ddb7161b7203c3334d8a4dff9d
thxCode <thxcode0824@gmail.com>
2025-02-25 12:13:39 +0800

    chore: tools
    [skip ci]
    
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- llama-box/tools/batch.sh
+++ llama-box/tools/batch.sh
@@ -36,7 +36,7 @@ function request() {
   for ((i=0; i<cc; i++))
   do
     idx=$(echo "$i % ${#user_contents[@]}" | bc)
-    curl -ks ${api_server}/v1/chat/completions \
+    curl -ks "${api_server}"/v1/chat/completions \
     -H "Content-Type: application/json" \
     -d "{\"temperature\":${temp},\"top_p\":${top_p},\"min_p\":${min_p},\"top_k\":${top_k},\"max_tokens\":${max_tokens},\"seed\":${seed},\"messages\":[{\"role\":\"user\",\"content\":\"${user_contents[$idx]}\"}]}" > "/tmp/response_$i.json" &
   done
@@ -48,7 +48,7 @@ function request() {
   else 
     end_time=$(date +%s%N)
   fi
-  tt=$((($end_time - $start_time) / 1000000))
+  tt=$(((end_time - start_time) / 1000000))
 
   # observe
   oks=$cc
@@ -76,16 +76,16 @@ function request() {
   tts=$((pts + dts))
 
   # result
-  tps=$(echo "scale=2; $tts * 1000 / $tt" | bc)
-  avg_pps=$(echo "scale=2; $pts * 1000 / $tt / $oks" | bc)
-  avg_dps=$(echo "scale=2; $dts * 1000 / $tt / $oks" | bc)
+  tps=$(echo "scale=2; $tts * 1000 / $tt" | bc 2>/dev/null)
+  avg_pps=$(echo "scale=2; $pts * 1000 / $tt / $oks" | bc 2>/dev/null)
+  avg_dps=$(echo "scale=2; $dts * 1000 / $tt / $oks" | bc 2>/dev/null)
   if [[ "${ppss}" != "0" ]]; then
-    avg_pps=$(echo "scale=2; $ppss / $oks" | bc)
+    avg_pps=$(echo "scale=2; $ppss / $oks" | bc 2>/dev/null)
   fi
   if [[ "${dpss}" != "0" ]]; then
-    avg_dps=$(echo "scale=2; $dpss / $oks" | bc)
+    avg_dps=$(echo "scale=2; $dpss / $oks" | bc 2>/dev/null)
   fi
-  printf " %2d (%2d) |%8d ms |%7d (%7d, %7d) |%9.2f tps |%9.2f tps |%9.2f tps \n" $cc $oks $tt $tts $pts $dts $tps $avg_pps $avg_dps
+  printf " %2d (%2d) |%8d ms |%7d (%7d, %7d) |%9.2f tps |%9.2f tps |%9.2f tps \n" "$cc" "$oks" $tt $tts $pts $dts "$tps" "$avg_pps" "$avg_dps"
 }
 
 echo "API_SERVER=${api_server} TEMP=${temp} TOP_P=${top_p} MIN_P=${min_p} TOP_K=${top_k} MAX_TOKENS=${max_tokens} SEED=${seed}"
